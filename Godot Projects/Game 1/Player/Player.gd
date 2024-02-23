1fb565108f1cdc74ee80c78a6dbe0050d95d9434
SammNic <nicoderms9805@gmail.com>
2024-02-23 06:34:05 -0500

    wait... im goated

--- Godot Projects/Game 1/Player/Player.gd
+++ Godot Projects/Game 1/Player/Player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,6 +13,10 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	if is_on_floor():
+		if Input.is_action_pressed("move_down"):
+			position.y += 1
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
